738f3449c8d00ba55df9c944b124081f805ae8f2
piyushpriyadarshidev26-source <piyushpriyadarshidev26@gmail.com>
2026-08-09 15:36:09 +0530

    Update module sources to point to Preprod directory

--- Environment/Preprod/main.tf
+++ Environment/Preprod/main.tf
@@ -1,11 +1,11 @@
 module "resource_group" {
-    source ="../Child_Module/azurerm_resource_group"
+    source ="../Preprod/Child_Module/azurerm_resource_group"
     rg=var.rg
   
 }
 
 module "storage_account" {
-  source = "../Child_Module/azurerm_storage_account"
+  source = "../Preprod/Child_Module/azurerm_storage_account"
   depends_on = [ module.resource_group ]
   stg=var.stg
 }
@@ -13,18 +13,18 @@ module "storage_account" {
 
 
 module "subnet" {
-  source = "../Child_Module/azurerm_subnet"
+  source = "../Preprod/Child_Module/azurerm_subnet"
   depends_on = [ module.resource_group,module.storage_account,module.virtual_network]
   snet=var.snet
 }
 
 module "public_ip"{
-    source = "../Child_Module/azurerm_public_ip"
+    source = "../Preprod/Child_Module/azurerm_public_ip"
     depends_on=[module.resource_group,module.storage_account,module.virtual_network,module.subnet]
     pip=var.pip
 }
 module "linux_virtual_machine" {
-    source = "../Child_Module/azurerm_virtual_machine"
+    source = "../Preprod/Child_Module/azurerm_virtual_machine"
     depends_on = [ module.resource_group,module.storage_account,module.virtual_network,module.subnet,module.public_ip ]
     vms=var.vms
   
